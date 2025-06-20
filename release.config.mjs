df773a12342542adb3e18f7386e0e583cc4b568f
murilosopi <dev.murilosopi@gmail.com>
2025-06-20 12:43:13 -0300

    chore: improve release rules for commit analyzer

--- release.config.mjs
+++ release.config.mjs
@@ -10,7 +10,16 @@ export default {
         preset: 'conventionalcommits',
         parserOpts: {
           noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING']
-        }
+        },
+        releaseRules: [
+          { type: 'chore', release: 'patch' },
+          { type: 'style', release: 'patch' },
+          { type: 'refactor', release: 'patch' },
+          { type: 'refactor', release: 'patch' },
+          { type: 'feat', release: 'minor' },
+          { type: 'fix', release: 'patch' },
+          { breaking: true, release: 'major' }
+        ]
       }
     ],
     [
