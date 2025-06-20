e52f860d2c6dd6261b6df7e768cc2c8a43788ee9
murilosopi <dev.murilosopi@gmail.com>
2025-06-20 04:58:15 -0300

    chore: add conventional commits using commit lint

---
+++ commitlint.config.mjs
@@ -0,0 +1,24 @@
+const config = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [2, 'always', 72],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'chore',
+        'revert'
+      ]
+    ],
+    'subject-case': [2, 'never', ['sentence-case', 'start-case']]
+  }
+};
+
+export default config;
